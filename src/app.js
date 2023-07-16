7bbbd847293f19de4dfadc0bb50a79c4617b72d9
Aminadad <aminadad77@hotmail.com>
2023-07-16 16:04:31 -0400

    add archivo

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const morgan  = require('morgan');
 const app = express();
 
 let port = process.env.PORT || 9090
+//usar middleware de morgan para ver las peticiones un mi consola
+app.use(morgan('dev'));
 
 app.get("/",(req, res)=>{
     res.send("Welcome");
+})
+app.get("/",(req, res)=>{
+    res.status(200).send("Welcome");
 })
 
 app.listen(port, function(){
     console.log(`listening on http://127.0.0.1:${port}`);
 });
-
